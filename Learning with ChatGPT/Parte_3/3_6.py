bfb21a3839ad0c750137dab16a2a6e91f9662471
Daniel <daniel.bezhin@gmail.com>
2025-10-11 12:14:05 +0400

    1.7

--- Learning with ChatGPT/Parte_3/3_6.py
+++ Learning with ChatGPT/Parte_3/3_6.py
@@ -1,2 +1,5 @@
 #scrivi un modulo che contiene la funzione per calcolare l'area di un cerchio 
 #pi * r^2
+import module
+r = int(input("Inserisci il raggio del cerchio: "))
+print("il risultato e`", module.circar(r))
---
+++ Learning with ChatGPT/Parte_3/module.py
@@ -0,0 +1,6 @@
+#scrivi un modulo che contiene la funzione per calcolare l'area di un cerchio 
+#pi * r^2
+def circar(r):
+    a = 3.14 * r * r
+    return a
+    
