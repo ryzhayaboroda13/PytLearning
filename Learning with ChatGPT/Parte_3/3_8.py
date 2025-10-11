708ad7107ab4cc1ea41738389233315476e65881
ryzhayaboroda13 <daniel.bezhin@gmail.com>
2025-10-11 13:34:19 +0400

    2.0

---
+++ Learning with ChatGPT/Parte_3/3_8.py
@@ -0,0 +1,4 @@
+#scrivi una funzione che controlla se il numero e' primo
+import module
+num = int(input("INserisci il numero da verificare: "))
+print(module.is_prime(num))
--- Learning with ChatGPT/Parte_3/module.py
+++ Learning with ChatGPT/Parte_3/module.py
@@ -3,3 +3,15 @@
 def areacerchio(r):
     area = 3.14 * r * r
     return area
+
+
+#scrivi una funzione che controlla se il numero e' primo
+def is_prime(num):
+    if num < 1:
+        return "Number isn't prime"
+    
+    for i in range(2, num):
+        if num % i == 0:
+            return "Number isn't prime"
+        
+    return "Number is prime"
