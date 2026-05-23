82a877f99aa82872fd1564925b269ff8b35f0db3
Daniel <daniel.bezhin@gmail.com>
2026-05-23 20:05:23 +0200

    kk

---
+++ Обучение с Deepseek #2/lesson_1.py
@@ -0,0 +1,23 @@
+#generiamo delle variabili
+
+saluto = 'ciao'
+nome = 'Marco'
+
+#ora visualizziamo il testo
+
+print(saluto, nome)
+
+#operazioni matematiche
+
+a = 12
+b = 23
+
+print('il risultato della somma e`', a+b)
+
+#cambio della variabile o l'assegnazione del nuovo valore
+
+nome = 'alessandro'
+print('\nora il nome e`', nome) #ho aggiunto un rigo vuoto tramite \n
+
+c = 10; d = 3; somma = c + d; 
+print('la somma di c=10 e d=3 e`',somma, '\n/', c/d, '\n*', c*d, '\n-', c-d, '\n//', c//d)
